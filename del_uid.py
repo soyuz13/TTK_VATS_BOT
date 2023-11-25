c02521ccdae305daa73df930e9d4fa09c1d8be18
Vadim <transit010@mail.ru>
2023-11-25 11:12:48 +1000

    add del_uid.py for test

---
+++ del_uid.py
@@ -0,0 +1,10 @@
+import main
+from config import DB_NAME
+main.get_sended_uids(DB_NAME)
+
+num = input("Enter uid-number for deleting from db: ")
+
+if num:
+    print(main.del_uid(num))
+else:
+    print("Nothing to delete")                                                                                    10,11         All
